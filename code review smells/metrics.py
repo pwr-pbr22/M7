32a4cd4002ee883c0938a5c830a759930b5db4ee
Szymon Zmijewski <sz.zmijewski24@gmail.com>
2022-05-17 01:14:27 +0200

    review metrics

--- code review smells/metrics.py
+++ code review smells/metrics.py
@@ -18,7 +18,7 @@ class Result:
             map(lambda r: float(r[0]) if r[0] is not None else None,
                 session.execute(select(column(self.metric_name)).select_from(self.evaluated.subquery())).all()))
         numeric_entries = list(filter(lambda e: e is not None, measures))
-        average = sum(numeric_entries)/len(numeric_entries) if len(numeric_entries) > 0 else float("nan")
+        average = sum(numeric_entries) / len(numeric_entries) if len(numeric_entries) > 0 else float("nan")
         return list(map(lambda e: e if e is not None else average, measures))
 
 
@@ -49,6 +49,45 @@ def review_window_per_line_metric(considered: Query, repo: Repository) -> Result
                                                  (
                                                          extract('epoch', PullRequest.closed_at) -
                                                          extract('epoch', PullRequest.created_at)
-                                                 ) / 60 / (PullRequest.additions+PullRequest.deletions))
+                                                 ) / 60 / (PullRequest.additions + PullRequest.deletions))
                                          ).label(name))
                   )
+
+
+def count_reviews_chards(reviews):
+    return sum(map(lambda r: r.review_chars, reviews))
+
+
+def review_chars(considered: Query, repo: Repository) -> Result:
+    name = "review_chars"
+    return Result(name,
+                  repo,
+                  considered,
+                  considered.add_columns((
+                                             count_reviews_chards(PullRequest.reviews)).label(name)))
+
+
+def review_chars_code_lines_ratio(considered: Query, repo: Repository):
+    name = "review_chars_per_loc"
+    return Result(name,
+                  repo,
+                  considered,
+                  considered.add_columns((
+                                                 count_reviews_chards(PullRequest.reviews) / (
+                                                 PullRequest.additions - PullRequest.deletions)).label(name)))
+
+
+def count_reviews_hours(reviews, creation_time):
+    return sum(map(lambda r: r.submitted_at - creation_time, reviews)) / len(reviews)
+
+
+def reviewed_lines_per_hour(considered: Query, repo: Repository):
+    name = "reviewed_lines_per_hour"
+    return Result(name,
+                  repo,
+                  considered,
+                  considered.add_columns((
+                                                 (
+                                                         PullRequest.additions - PullRequest.deletions) /
+                                                 count_reviews_hours(PullRequest.reviews,
+                                                                     PullRequest.created_at)).label(name)))
