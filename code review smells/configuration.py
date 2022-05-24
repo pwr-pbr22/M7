9feb7a84b0cfa2ff171122e8f9a9b826b676a569
damienthewave <garbalad@gmail.com>
2022-05-24 12:22:18 +0200

    Download multiple projects at one run

--- code review smells/configuration.py
+++ code review smells/configuration.py
@@ -5,6 +5,6 @@ class ProjectConfiguration:
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config.read('config.properties')
-        self.project = self.config.get("Config", "project")
+        self.projects = self.config.get("Config", "projects").split(",")
         self.connstr = self.config.get("Config", "connstr")
         self.gh_keys = self.config.get("Config", "gh_keys").split(",")
